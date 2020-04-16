e3678c0e52eb5c44dc38025a734c77eab7e4440c
kyle-nedbal <kylenedbal88@gmail.com>
2020-04-16 16:26:08 -0400

    do while loop

---
+++ Exercise6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Hello, World!";
+            string answer = "y";
+            do
+            {
+                Console.WriteLine(greeting);
+                Console.WriteLine("Would You like to continue? (y/n)");
+                answer = Console.ReadLine();
+            } while (answer == "y");
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
